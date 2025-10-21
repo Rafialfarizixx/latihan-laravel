d41c471bd5a683eb152cd3ad77fe8b124154c06a
Rafialfarizixx <mr.alfarizi04@gmail.com>
2025-10-21 08:59:29 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return 'Hello, Rafi';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return "Nama Saya $name";
+});
+
+Route::get('/greet/{name?}', function ($name = 'Guest') {
+    return "Halo, $name";
+});
+
+Route::get('/profile', function () {
+    return view('profile');
+});
+
+Route::get('/about', function () {
+    return view('about', ['name' => 'Rafi']);
+});
+
+Route::get('/home', function () {
+    return 'Halo, Ini adalah halaman Home';
+})->name('home.page');
