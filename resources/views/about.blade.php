2aa55475e25f22f91598fd053ba66a69bf02c417
Rafialfarizixx <mr.alfarizi04@gmail.com>
2025-10-21 09:14:28 +0700

    perbaikan dashboard

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,10 @@
+
+<slot name="header">
+    <h2 class="font-semibold text-xl text-gray-900 leading-tight">
+        {{ __('Dashboard' )}}
+    </h2>
+</slot>
+
 <h1>About Page</h1>
 <p>Nama : {{ $name }} </p>
 
